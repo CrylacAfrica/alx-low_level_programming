44fd10d6bb15b84683f9f7354a78ffa9b23afcda
CrylacAfrica <dgitonga99@gmail.com>
2022-09-10 12:51:34 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+
 /**
- * Main - prints text
+ * Main - prints text in put function
  *
  * Description - Main prints 'with proper grammar, but the outcome is a piece of art,'
- *
- * Returns 0
+ * Return: 0
  */
 int main(void)	
 {
 
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }

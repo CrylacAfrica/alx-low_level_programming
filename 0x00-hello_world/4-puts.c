4e98c5d4d03bdc7de00920391dc3db3e3e4d9cb6
CrylacAfrica <dgitonga99@gmail.com>
2022-09-10 12:21:54 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 	/**
 	 * Main: Output string
 	 *
-	 * Desc: Prints "Programming is like building a multilingual puzzle"
+	 * Description: Prints "Programming is like building a multilingual puzzle"
 	 *
 	 * Return: (0)- sucess.
 	 */

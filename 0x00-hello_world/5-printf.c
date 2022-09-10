cbe9271091dbccca8992353323d01e4859435c67
CrylacAfrica <dgitonga99@gmail.com>
2022-09-10 12:45:05 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-	/**
-	 * Main - prints text
-	 *
-	 * Description - Main prints 'with proper grammar, but the outcome is a piece of art,'
-	 *
-	 * Returns 0
-	 */
-	int main(void)	
-	{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
-	
-	}
+/**
+ * Main - prints text
+ *
+ * Description - Main prints 'with proper grammar, but the outcome is a piece of art,'
+ *
+ * Returns 0
+ */
+int main(void)	
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+}

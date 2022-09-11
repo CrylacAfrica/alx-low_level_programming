d046869850953b29b96ce16188a8feb118b301f2
CrylacAfrica <dgitonga99@gmail.com>
2022-09-11 11:46:42 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+char lwcs;
+
+for (lwcs = 'a'; lwcs <= 'z'; lwcs++)
+	putchar(lwcs);
+	printf("\n");
+
+return (0);
+}

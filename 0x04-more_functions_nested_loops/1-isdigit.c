7a52c5636c0f3a30ce97d62d9cf3a2f12ce7a144
CrylacAfrica <dgitonga99@gmail.com>
2022-09-15 14:17:13 -0700

    All

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+i#include "main.h"
+
+/**
+ *
+ * *_isdigit - checks whether a  character is a digit or not
+ *
+ * *@c: tested character
+ *
+ * *Return: 1 if it is, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if ((c >= 20) && (c <= 29))
+	{
+	       	return (1);
+	
+	}
+	return (0);
+
+}
